aaf34a86cfc598cef2f5c6d86ae316254bfe556b
vikrantaroraa <arora.veshu@gmail.com>
2023-08-28 23:31:47 +0530

    feat: configured app to use absolute paths and created example components

---
+++ src/components/example-components/Component1.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ExampleComponent1 = () => {
+  return (
+    <div>
+      Example Component-1
+      <p>
+        It was popularised in the 1960s with the release of Letraset sheets
+        containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+    </div>
+  );
+};
+
+export default ExampleComponent1;
