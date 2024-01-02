3740cda8b106cfc6237e92d0d9d73f14374b963f
vikrantaroraa <arora.veshu@gmail.com>
2024-01-02 23:05:18 +0530

    refactor: refactored the code and simplified the Timeline component implementation

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -10,82 +10,32 @@ const Timeline = ({
 }: TimelineProps) => {
   return (
     <div className={styles["timeline"]} style={{ gap: timelineGap }}>
-      {data?.map((entry, index) => {
-        return (
-          <div className={styles["timeline-entry"]}>
-            <div
-              className={styles["image-container"]}
-              style={{ width: dotSize }}
-            >
-              <img src="https://source.unsplash.com/iEEBWgY_6lA" />
-              {index !== data.length - 1 && (
-                <div
-                  className={styles["line"]}
-                  style={{
-                    top: dotSize,
-                    bottom: `-${timelineGap}px`,
-                  }}
-                ></div>
-              )}
-            </div>
-            {/* The data contains the objects that are passed as prop objects to the UserTimelineComponent  */}
-            {UserTimelineComponent && data ? (
-              <UserTimelineComponent
-                notificationTitle={entry.notificationTitle}
-                time={entry.time}
-                content={entry.content}
-              />
-            ) : (
-              <div className={styles["timeline-card"]}>
-                <div className={styles["header"]}>
-                  <div>Rose commented on your post!</div>
-                  <div>7:00 am</div>
-                </div>
-                {index % 2! === 0 && (
-                  <div className={styles["content"]}>
-                    The photo of you and the dog was liked by John. Lorem ipsum
-                    dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua.
-                  </div>
+      {/* the timelineData extracted inside map is the prop object to be passed to the <UserTimelineComponent />  */}
+      {UserTimelineComponent &&
+        data &&
+        data?.map((timelineData, index) => {
+          return (
+            <div className={styles["timeline-entry"]} key={index}>
+              <div
+                className={styles["image-container"]}
+                style={{ width: dotSize }}
+              >
+                <img src="https://source.unsplash.com/iEEBWgY_6lA" />
+                {index !== data.length - 1 && (
+                  <div
+                    className={styles["line"]}
+                    style={{
+                      top: dotSize,
+                      bottom: `-${timelineGap}px`,
+                    }}
+                  ></div>
                 )}
               </div>
-            )}
-          </div>
-        );
-      })}
-      {/* STATIC DATA STARTS */}
-      {/* timeline-entry - 1 */}
-      {/* <div className={styles["timeline-entry"]}>
-        <div className={styles["image-container"]} style={{ width: dotSize }}>
-          <img src="https://source.unsplash.com/iEEBWgY_6lA" />
-          <div
-            className={styles["line"]}
-            style={{
-              top: dotSize,
-              bottom: `-${timelineGap}px`,
-            }}
-          ></div>
-        </div>
-        <UserTimelineComponent />
-      </div> */}
-      {/* timeline-entry - 2 */}
-      {/* <div className={styles["timeline-entry"]}>
-        <div className={styles["image-container"]} style={{ width: dotSize }}>
-          <img src="https://source.unsplash.com/iEEBWgY_6lA" />
-          <div
-            className={styles["line"]}
-            style={{ top: dotSize, bottom: `-${timelineGap}px` }}
-          ></div>
-        </div>
-        <UserTimelineComponent />
-      </div> */}
-      {/* timeline-entry - 3 */}
-      {/* <div className={styles["timeline-entry"]}>
-        <div className={styles["image-container"]} style={{ width: dotSize }}>
-          <img src="https://source.unsplash.com/iEEBWgY_6lA" />
-        </div>
-        <UserTimelineComponent />
-      </div> */}
+              {/* The data array received as prop contains the objects that are passed as prop objects to the UserTimelineComponent  */}
+              <UserTimelineComponent {...timelineData} />
+            </div>
+          );
+        })}
     </div>
   );
 };
--- src/components/example-components/TimelineExample/index.tsx
+++ src/components/example-components/TimelineExample/index.tsx
@@ -2,34 +2,36 @@ import React from "react";
 import { Timeline } from "src/components/Timeline";
 import UserTimelineComponent from "src/components/dummy-components/UserTimelineComponent";
 
-const timelineData = [
+const timelineDataArray = [
   {
     notificationTitle: "John liked your photo",
     time: "8:25 am",
-    content:
-      "The photo of you and the dog was liked by John. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    content: "The photo of you and the dog was liked by John.",
   },
   {
     notificationTitle: "Emma commented on your post",
     time: "9:00 am",
-    content:
-      "Emma left a comment on your post. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    content: "Emma left a comment on your post.",
+  },
+  {
+    notificationTitle: "Rose sent you a message",
+    time: "11:42 am",
+    content: "Hi Vikrant, I have been thinking about you.",
   },
   {
     notificationTitle: "Natalie liked your photo",
-    time: "11:42 am",
-    content:
-      "Your photo was liked by Natalie. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet.",
+    time: "2:54 pm",
+    content: "Your photo was liked by Natalie.",
   },
 ];
 
 const ExampleTimeline = () => {
   return (
     <Timeline
-      data={timelineData}
-      dotSize={50}
+      data={timelineDataArray}
+      dotSize={40}
       timelineGap={16}
-      // UserTimelineComponent={(props) => <UserTimelineComponent {...props} />}
+      UserTimelineComponent={(props) => <UserTimelineComponent {...props} />}
     />
   );
 };
